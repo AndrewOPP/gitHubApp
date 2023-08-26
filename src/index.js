19bed8ac6b6f2272d0a5afd0b8e53c458c28b459
AndrewOPP <a.vyblov2003@gmail.com>
2023-08-26 16:14:21 +0300

    Done

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import throttle from 'lodash.throttle'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const mainContent = document.querySelector(".main-content")
+const input = document.querySelector(".user-input")
+input.addEventListener("change", findInfo)
+let numberOfRepos = 0
+    
+function findInfo(event) {
+    userLogin = event.target.value
+    fetch(`https://api.github.com/users/${userLogin}`)
+        .then(response => {
+            if (!response.ok) throw new Error(Notify.failure('User does not exist'))
+            
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            createMarkup(data)
+            repos(userLogin)
+        })
+        .catch(error => {
+        console.log(error);
+    })
+}
+
+async function repos(login) {
+    const link = `https://api.github.com/users/${login}` + `/repos`
+
+    const resp = await fetch(link)
+    const respData = await resp.json()
+    console.log(respData);
+    addReposToMarkup(respData)
+}
+
+const defElements = {
+    profileName: "profile name",
+    bio: "Profile bio is empty"
+}
+
+
+
+
+
+
+
+
+
+function createMarkup({ login, followers, following, bio, avatar_url, public_repos}) {
+    const markUp = `
+        <img src="${avatar_url}" alt="profile-photo" class="profile-foto" />
+        <div class="profile-information-container">
+          <h2 class="profile name">${login || defElements.profileName}</h2>
+          <p class="profile-bio">${bio || defElements.bio}</p>
+          <ul class="stats">
+            <li class="stats-li">${followers} Followers</li>
+            <li class="stats-li">${following} Following</li>
+            <li class="stats-li">${public_repos} Repos</li>
+          </ul>
+          <h3 class="repos-title">Repos:</h3>
+          <div class="repos" id="repos"></div>
+        </div>`
+    
+    setTimeout(() => {
+        mainContent.innerHTML = markUp
+        mainContent.classList.add("add-main-content")
+    }, 300)
+    
+}
+
+
+function addReposToMarkup(respData) {
+    const reposEl = document.getElementById("repos")
+    console.log(reposEl);
+    respData.slice(0, 10).forEach(element => {
+        const ell = document.createElement("a")
+        ell.classList.add("repo")
+        ell.href = element.html_url
+        ell.target = "_blank"
+        ell.innerText = element.name
+
+        reposEl.appendChild(ell)
+    });
+}
