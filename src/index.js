8d02717db1f7015aa60c9ef4efda38c79e668e1e
AndrewOPP <a.vyblov2003@gmail.com>
2023-08-26 17:43:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,56 +1,39 @@
-import throttle from 'lodash.throttle'
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const mainContent = document.querySelector(".main-content")
 const input = document.querySelector(".user-input")
 input.addEventListener("change", findInfo)
-let numberOfRepos = 0
+const h1 = document.querySelector("h1")
     
 function findInfo(event) {
     userLogin = event.target.value
     fetch(`https://api.github.com/users/${userLogin}`)
         .then(response => {
             if (!response.ok) throw new Error(Notify.failure('User does not exist'))
-            
             return response.json()
         })
         .then(data => {
-            console.log(data);
             createMarkup(data)
             repos(userLogin)
         })
         .catch(error => {
-        console.log(error);
-    })
+            console.log(error);
+        })
 }
 
 async function repos(login) {
     const link = `https://api.github.com/users/${login}` + `/repos`
-
     const resp = await fetch(link)
     const respData = await resp.json()
-    console.log(respData);
     addReposToMarkup(respData)
 }
-
-const defElements = {
-    profileName: "profile name",
-    bio: "Profile bio is empty"
-}
-
-
-
-
-
-
-
-
 
 function createMarkup({ login, followers, following, bio, avatar_url, public_repos}) {
     const markUp = `
         <img src="${avatar_url}" alt="profile-photo" class="profile-foto" />
         <div class="profile-information-container">
-          <h2 class="profile name">${login || defElements.profileName}</h2>
-          <p class="profile-bio">${bio || defElements.bio}</p>
+          <h2 class="profile name">${login}</h2>
+          <p class="profile-bio">${bio || "Profile bio is empty"}</p>
           <ul class="stats">
             <li class="stats-li">${followers} Followers</li>
             <li class="stats-li">${following} Following</li>
@@ -60,24 +43,25 @@ function createMarkup({ login, followers, following, bio, avatar_url, public_rep
           <div class="repos" id="repos"></div>
         </div>`
     
+    h1.classList.add('hidden')
+
     setTimeout(() => {
         mainContent.innerHTML = markUp
         mainContent.classList.add("add-main-content")
-    }, 300)
+    }, 1000)
     
 }
 
-
 function addReposToMarkup(respData) {
-    const reposEl = document.getElementById("repos")
-    console.log(reposEl);
-    respData.slice(0, 10).forEach(element => {
+    setTimeout(() => {
+        const reposEl = document.getElementById("repos")
+        respData.slice(0, 10).forEach(element => {
         const ell = document.createElement("a")
         ell.classList.add("repo")
         ell.href = element.html_url
         ell.target = "_blank"
         ell.innerText = element.name
-
         reposEl.appendChild(ell)
-    });
+    });  
+    }, 1000)
 }
